1fdedffd13f343e448976a2f61a9da6693f5d3f7
stanislavmarochok <ssssstas30@gmail.com>
2022-10-28 17:42:13 +0200

    test change 2

--- src/scripts/DataManager.js
+++ src/scripts/DataManager.js
@@ -1,7 +1,38 @@
 class DataManager extends React.Component {
-    render() {
-      return <h2>Hi, I am a Car!</h2>;
-    }
+
+  static getItems(){
+    return [
+      {
+        id: 1,
+        name: "Tony",
+        address: "HJK",
+        price: 20000,
+        flatsCount: 2,
+        currency: "USD"
+      },
+      {
+        id: 2,
+        name: "Andrew",
+        address: "sgdfdf",
+        price: 30000,
+        flatsCount: 3,
+        currency: "EUR"
+      },
+      {
+        id: 3,
+        name: "Alex",
+        address: "uiop",
+        price: 30000,
+        flatsCount: 3,
+        currency: "EUR"
+      }
+    ];
+  }
+
+
+  render() {
+    return <h2>Hi, I am a Car!</h2>;
+  }
 }
 
 export default DataManager;
